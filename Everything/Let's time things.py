ad07930111458632563c1c0e3611495680863c68
OehiAlm <lucky-pieper@web.de>
2021-01-17 20:52:13 +0100

    Doing something 100 times

--- Everything/Let's time things.py
+++ Everything/Let's time things.py
@@ -2,6 +2,7 @@ from time import time
 from tomsTestRange import uplo
 
 start = time()
-print("We have {0[0]} upper case letters, {0[1]} lower case letters".format(uplo("test_test_test")))
+for x in range(100):
+    print("We have {0[0]} upper case letters, {0[1]} lower case letters".format(uplo("test_test_test")))
 end = time()
 print(f'It took {end - start} seconds to run this!')
