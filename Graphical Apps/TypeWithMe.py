c2e6f71d39653f9c95bc8eac5318758d4ae79908
Irodyy <76697688+Irodyy@users.noreply.github.com>
2023-01-05 20:05:12 +0100

    features

---
+++ Graphical Apps/TypeWithMe.py
@@ -0,0 +1,18 @@
+import random
+import os
+import timeit
+
+#TypeWithMe
+
+#Features
+# - Language selection (Deutsch / English)
+# - Pull sentences from webpage database based on selection
+# - Define 'difficulty' of sentence by analysing length & complexity
+# - Countdown until typing input is accepted so player can get ready
+# - Time and analyze typing
+# - Give visual feedback for typing errors by marking mistakes red
+# - Show progress bar and %
+# - Count backspace corrections
+# - When sentence is done show results by calculating a grade
+# - Retry or New sentence/language
+
